7b5082bb368277ee12dbd8a04e9a383be915740c
Neeraj Shrivastava <neeraj.shrivastava@sas.com>
2023-11-26 11:13:06 -0500

    Initial commit

---
+++ cardio_UI/main.py
@@ -0,0 +1,69 @@
+from preprocessing import DataPreprocessor
+import streamlit as st
+import pickle
+import pandas as pd
+
+#with open('finalized_model.sav', 'rb') as file:
+#trained_model = pickle.load(file)
+
+
+preprocessor = DataPreprocessor()
+
+# stremlit app
+st.title("Cardio Prediction App")
+
+# Collect user inputs
+st.sidebar.header("User Input:")
+age = st.sidebar.slider("Age", 0, 100, 50)
+gender = st.sidebar.selectbox("Gender", ["Male", "Female"])
+height = st.sidebar.slider("Height (cm)", 0, 300, 165)
+weight = st.sidebar.slider("Weight (kg)", 0, 300, 70)
+ap_hi = st.sidebar.slider("Systolic Blood Pressure (ap_hi)", 50, 250, 120)
+ap_lo = st.sidebar.slider("Diastolic Blood Pressure (ap_lo)", 30, 200, 80)
+cholesterol = st.sidebar.selectbox("Cholesterol Level", ["Normal", "Above Normal", "High"])
+gluc = st.sidebar.selectbox("Glucose Level", ["Normal", "Above Normal", "High"])
+smoke = st.sidebar.selectbox("Smoking", ["No", "Yes"])
+alco = st.sidebar.selectbox("Alcohol Consumption", ["No", "Yes"])
+active = st.sidebar.selectbox("Physical Activity", ["No", "Yes"])
+
+# Map user inputs to data
+gender_mapping = {"Male": 0, "Female": 1}
+cholesterol_mapping = {"Normal": 1, "Above Normal": 2, "High": 3}
+gluc_mapping = {"Normal": 1, "Above Normal": 2, "High": 3}
+smoke_mapping = {"No": 0, "Yes": 1}
+alco_mapping = {"No": 0, "Yes": 1}
+active_mapping = {"No": 0, "Yes": 1}
+
+user_input = {
+    'age': age,
+    'gender': gender_mapping[gender],
+    'height': height,
+    'weight': weight,
+    'ap_hi': ap_hi,
+    'ap_lo': ap_lo,
+    'cholesterol': cholesterol_mapping[cholesterol],
+    'gluc': gluc_mapping[gluc],
+    'smoke': smoke_mapping[smoke],
+    'alco': alco_mapping[alco],
+    'active': active_mapping[active]
+}
+
+user_input_data = pd.DataFrame(user_input, index=[0])
+
+
+final_df = preprocessor.preprocess_and_cluster(user_input_data)
+
+# make predictions
+predictions = trained_model.predict(final_df)
+
+st.sidebar.header("Cardio Prediction: ")
+if predictions[0] == 1:
+    st.sidebar.warning("Risk of Cardiovascular Disease Detected!")
+else:
+    st.sidebar.success("No Cardiovascular Disease Detected")
+
+
+
+
+
+
---
+++ cardio_UI/preprocessing.py
@@ -0,0 +1,106 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn import preprocessing
+from kmodes.kmodes import KModes
+import pickle
+
+class DataPreprocessor:
+    def __init__(self):
+        self.le = preprocessing.LabelEncoder()
+        with open('female_clusters.pkl', 'rb') as file:
+            self.kmodes_female = pickle.load(file)
+
+        with open('male_clusters.pkl', 'rb') as file:
+            self.kmodes_male = pickle.load(file)
+
+    def preprocess_data(self, data):
+        # Calculate 'age_bin' based on provided bin_range and bin_labels
+        bin_range = [0, 20, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+        bin_labels = [f'{bin_start}-{bin_end}' for bin_start, bin_end in zip(bin_range[:-1], bin_range[1:])]
+        data['age_bin'] = pd.cut(data['age'], bins=bin_range, labels=bin_labels)
+        data['bmi'] = data['weight'] / ((data['height'] / 100) ** 2)
+
+        # Calculate 'BMI_Class' based on BMI calculations
+        bmi_ratings = []
+
+        for row in data['bmi']:
+            if row < 18.5:
+                bmi_ratings.append(1)  # Underweight
+            elif 18.5 <= row < 24.9:
+                bmi_ratings.append(2)  # Normal Weight
+            elif 24.9 <= row < 29.9:
+                bmi_ratings.append(3)  # Overweight
+            elif 29.9 <= row < 34.9:
+                bmi_ratings.append(4)  # Class Obesity 1
+            elif 34.9 <= row < 39.9:
+                bmi_ratings.append(5)  # Class Obesity 2
+            elif 39.9 <= row < 49.9:
+                bmi_ratings.append(6)  # Class Obesity 3
+            else:
+                bmi_ratings.append('Error')
+        data['BMI_Class'] = bmi_ratings
+
+        # Calculate 'MAP' and 'MAP_Class' based on provided logic
+        data['MAP'] = ((2 * data['ap_lo']) + data['ap_hi']) / 3
+        map_values = []
+        for row in data['MAP']:
+            if row < 69.9:
+                map_values.append(1)  # Low
+            elif 70 <= row < 79.9:
+                map_values.append(2)  # Normal
+            elif 79.9 <= row < 89.9:
+                map_values.append(3)  # Normal
+            elif 89.9 <= row < 99.9:
+                map_values.append(4)  # Normal
+            elif 99.9 <= row < 109.9:
+                map_values.append(5)  # High
+            elif 109.9 <= row < 119.9:
+                map_values.append(6)  # Normal
+            else:
+                map_values.append(7)
+        data['MAP_Class'] = map_values
+
+        # Drop the original columns that were used to create the features
+        data = data.drop(['age', 'weight', 'height', 'ap_hi', 'ap_lo', 'bmi'], axis=1)
+
+        return data
+
+    def preprocess_and_cluster(self, data):
+        df_cat = self.preprocess_data(data)
+
+        # Encode categorical features using LabelEncoder
+        df_cat_encoded = df_cat.apply(self.le.fit_transform)
+
+        # Separate data by gender
+        # df_male = df_cat_encoded[df_cat_encoded['gender'] == 0]
+        # df_female = df_cat_encoded[df_cat_encoded['gender'] == 1]
+
+        gender = df_cat_encoded['gender'].iloc[0]
+
+        if gender == 0:
+            clusters = self.kmodes_male.predict(df_cat_encoded)
+            df_cat_encoded.insert(0, 'Cluster', clusters, True)
+        elif gender == 1:
+            clusters = self.kmodes_female.predict(df_cat_encoded)
+            df_cat_encoded.insert(0, 'Cluster', clusters, True)
+        else:
+            raise ValueError("Invalid gender value. Expected 0 for male or 1 for female.")
+
+        #
+        #
+        # # Apply K-Modes clustering to each gender-specific dataset
+        # clusters_huang_1 = self.kmodes.fit_predict(df_female)
+        # df_female.insert(0, 'Cluster', clusters_huang_1, True)
+        #
+        # clusters_huang_2 = self.kmodes.fit_predict(df_male)
+        # df_male.insert(0, 'Cluster', clusters_huang_2, True)
+
+        # Concatenate the results
+        df_clusters = df_cat_encoded
+
+        return df_clusters[
+            ['Cluster', 'gender', 'age_bin', 'BMI_Class', 'MAP_Class', 'cholesterol', 'gluc', 'smoke', 'active']]
+
